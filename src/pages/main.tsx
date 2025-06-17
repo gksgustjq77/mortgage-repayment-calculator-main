86a958e2305d6f9606c6cfabae68543281999b33
gksgustjq77 <gksgustjq77@gmail.com>
2025-06-17 18:57:15 +0900

    미사용 코드 제거

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,13 +1,10 @@
 import Calculator from "../components/Calculator";
-import Result from "../components/Result";
 
 const Main: React.FC = () => {
   return (
     <>
       <div className="flex sm:flex-col items-stretch justify-center sm:rounded-none bg-white md:rounded-[20px] overflow-hidden shadow-md">
         <Calculator></Calculator>
-
-        {/* <Result resultType={""}></Result> */}
       </div>
     </>
   );
